cd337cad62952215a0c822f16ed0d503bdd65d6e
chrislee <657143946@qq.com>
2015-10-10 15:20:24 +0800

    添加一个职能分词器

--- JGraduate/src/main/java/com/shulianxunying/graduate/lucene/manager/LuceneConstant.java
+++ JGraduate/src/main/java/com/shulianxunying/graduate/lucene/manager/LuceneConstant.java
@@ -1,9 +1,13 @@
 package com.shulianxunying.graduate.lucene.manager;
 
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
 /**
  * Created by chrislee on 15-10-9 下午4:09.
  */
 public class LuceneConstant {
     public static final String INDEX_PATH = "E:\\workspaces\\GraduateCareerPlanning\\JGraduate\\index";
     public static final IndexManager index = new IndexManager(INDEX_PATH);
+
+    public static final IKAnalyzer ANALYZER = new IKAnalyzer(true);
 }
