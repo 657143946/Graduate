f9ad8539505b37d64b6bcfb05581fc4f436dd49e
chrislee <657143946@qq.com>
2015-10-12 14:26:48 +0800

    增加一个方法，实体转字符串

--- JGraduate/src/main/java/com/shulianxunying/graduate/utils/JsonUtils.java
+++ JGraduate/src/main/java/com/shulianxunying/graduate/utils/JsonUtils.java
@@ -9,4 +9,8 @@ public class JsonUtils {
     public static <T> T toEntity(String json, Class<T> clazz){
         return JSON.parseObject(json, clazz);
     }
+
+    public static String toJson(Object entity){
+        return JSON.toJSONString(entity);
+    }
 }
